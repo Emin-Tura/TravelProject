67f223dbdeda8bfc1cf5f48bd2e3a9c283fa3a3e
Muhammed Emin  TURA <muhammed.tura@ogr.sakarya.edu.tr>
2022-09-08 17:02:25 +0300

    Server Taraflı Silme İşlemi

--- server/models/Room.js
+++ server/models/Room.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
-const roomSchema = new mongoose.Schema(
+const roomSchema = mongoose.Schema(
   {
     lng: { type: Number, required: true },
     lat: { type: Number, required: true },
     price: { type: Number, min: 0, max: 50, default: 0 },
-    title: { type: String, required: true, minLength: 6, maxLength: 150 },
+    title: { type: String, required: true, minLength: 5, maxLength: 150 },
     description: {
       type: String,
       required: true,
@@ -18,11 +18,11 @@ const roomSchema = new mongoose.Schema(
     },
     uid: { type: String, required: true },
     uName: { type: String, required: true },
-    uPhoto: { type: String, required: "" },
+    uPhoto: { type: String, default: "" },
   },
-  { timeStamps: true }
+  { timestamps: true }
 );
 
-const Room = mongoose.model("Room", roomSchema);
+const Room = mongoose.model("rooms", roomSchema);
 
 export default Room;
